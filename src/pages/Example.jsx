abd986cea9bb66e6025c6ae328c874efd9d6c1fe
Matheus A Lino <matheus_araujolino@hotmail.com>
2022-12-09 00:47:12 -0500

    [add] improve styles

--- src/pages/Example.jsx
+++ src/pages/Example.jsx
@@ -14,37 +14,31 @@ const Example = () => {
         <span>A simple component built for ReactJS Apps</span>
       </header>
       <section className={styles.row}>
-        <div className={styles.col}>
-          {/* Example with a binary input */}
-          <Wrapper>
-            <Display digit={0b0} />
-            <code>{'<Display digit={0b0} />'}</code>
-          </Wrapper>
-
-          {/* Example with a hexadecimal input */}
-          <Wrapper>
-            <Display digit={0x1} />
-            <code>{'<Display digit={0b0} />'}</code>
-          </Wrapper>
-
-          {/* Example with an octal input */}
-          <Wrapper>
-            <Display digit={0o2} />
-            <code>{'<Display digit={0b0} />'}</code>
-          </Wrapper>
-
-          {/* Example with a decimal input */}
-          {[3, 4, 5, 6, 7, 8, 9].map((el, key) => (
-            <Wrapper>
-              <Display key={key} digit={el} />
+        <div className={`${styles.col} ${styles['col-A']}`}>
+          {/* Examples with binary, hexadecimal, octal and decimal inputs */}
+          {[
+            0,
+            1,
+            2,
+            '0b011',
+            '0b100',
+            '0b101',
+            '0x6',
+            '0x7',
+            '0x8',
+            '0o11',
+            'null',
+          ].map((el, key) => (
+            <Wrapper key={key}>
+              <Display digit={Number(el).toString(16)} />
               <code>{`<Display digit={${el}} />`}</code>
             </Wrapper>
           ))}
         </div>
-        <div className={styles.col}>
+        <div className={`${styles.col} ${styles['col-B']}`}>
           {[0xa, 0xb, 0xc, 0xd, 0xe, 0xf].map((el, key) => (
-            <Wrapper>
-              <Display key={key} digit={el} />
+            <Wrapper key={key}>
+              <Display digit={el} />
               <code>{`<Display digit={0x${el.toString(16)}} />`}</code>
             </Wrapper>
           ))}
